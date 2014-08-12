d2a67ec285e95ad315bb9722f785281e8fa7dc30
310u8 <m.310u8@gmail.com>
2014-08-12 18:04:40 +0900

    もろもろ

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = "blog"
 
-  blog.permalink = "{title}.html"
+  blog.permalink = "{title}"
   # Matcher for blog source files
   blog.sources = "{title}.html"
   # blog.taglink = "tags/{tag}.html"
